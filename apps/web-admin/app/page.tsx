9bdc21b310155c096ef457f880ac8570687a611d
atman-33 <gpbjk0304@gmail.com>
2023-08-20 14:33:23 +0900

    minor commit

--- apps/web-admin/app/page.tsx
+++ apps/web-admin/app/page.tsx
@@ -4,5 +4,5 @@ export default async function Index() {
    *
    * Note: The corresponding styles are in the ./index.css file.
    */
-  return <div className="test text-yellow-500">Hello world</div>;
+  return <div className=" text-yellow-500">Hello web-admin</div>;
 }
