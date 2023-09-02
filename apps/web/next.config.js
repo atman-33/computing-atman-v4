3eff5c8633d9473cf1983f2d29f8ecbe500c0aa3
atman-33 <gpbjk0304@gmail.com>
2023-09-03 00:45:46 +0900

    improve complile

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,9 @@
 const { composePlugins, withNx } = require('@nx/next');
 const { withContentlayer } = require('next-contentlayer');
 
+const dns = require('node:dns');
+dns.setDefaultResultOrder('ipv4first');
+
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
@@ -20,7 +23,8 @@ const nextConfig = {
         hostname: String(process.env.ATMAN_WEB_STORAGE_HOSTNAME)
       }
     ]
-  }
+  },
+  swcMinify: true
 };
 
 const plugins = [
